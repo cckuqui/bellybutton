8f26eb03d6a202d19ac39f92a3b4cc13533c1313
Cris B <cristina.bardan@gmail.com>
2020-05-12 18:46:03 -0500

    onchange function

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-// let dropDown = d3.select('#selDataSet');
+// Add names into dropdown
+function init() {
+    d3.json('samples.json').then((d) => {
+        var names = d.names;
+        var sel_id = d3.select('#selDataset')
+        // console.log(names);
+    
+        names.forEach((n) => {
+            // console.log(n);
+            sel_id
+                .append('option')
+                .text(n)
+                .property('value', n);
+            });    
+})}
 
-// function optionChanged (Tabledata){
-//     Tabledata.ForEach((number) => {
-//         let options = DataSetID.append('option');
-//     }) 
-// };
-// Open Json file with data.
-d3.json('../samples.json').then((data) =>{
-    console.log(data);
-    var d = data;
-});
+nameID = 940;
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+function optionChanged () {
+    var ddMenu = d3.select("#selDataset");
+    var nameID = ddMenu.property("value");
+    d3.json('samples.json').then((d) => {
+        var data = d.metadata;
+        var sample = d.samples;
+        var metadata = data.filter(data => data.id == nameID);
+        var samples = sample.filter(data => data.id == nameID);
+    });    
+}
+
+function table (metadata) {
+
+}
+
+init();
